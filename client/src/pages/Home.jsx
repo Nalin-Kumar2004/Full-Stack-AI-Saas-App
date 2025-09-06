e20504642f23f1b73e6a166372aa767bcf2508d2
Nalin-Kumar2004 <nalinnow@gmail.com>
2025-09-06 21:51:51 +0530

    Implement NavBar component and update Home page to include it

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
+
 
 const Home = () => {
   return (
-    <div>
-    <h1>Home Sweet Home!!!!</h1>
-    </div>
+    <>
+        <NavBar />
+    </>
   )
 }
 
